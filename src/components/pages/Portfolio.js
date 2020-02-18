e6b1814b84316add436dd5144eb67d3971893fc9
Maura Slavin <MauraMSlavin@gmail.com>
2020-02-17 21:04:51 -0500

    Maps over Project components.

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -24,62 +24,17 @@ function Portfolio() {
         <h2 className="text-center m-3">Portfolio</h2>
         <div className="icons text-light mb-2">
 
-          <Project 
-            name={projects[0].name}
-            projectLink={projects[0].projectLink}
-            githubLink={projects[0].githubLink}
-            image={icons[0]} 
-            alt={projects[0].alt}
-          />
+          {projects.map((project, index) => (
+            <Project  
+              name={project.name}
+              projectLink={project.projectLink}
+              githubLink={project.githubLink}
+              image={icons[index]} 
+              alt={project.alt}
+            />
 
-          <Project 
-            name={projects[1].name}
-            projectLink={projects[1].projectLink}
-            githubLink={projects[1].githubLink}
-            image={icons[1]}
-            alt={projects[1].alt}
-          />
+          ))}
 
-          <Project 
-            name={projects[2].name}
-            projectLink={projects[2].projectLink}
-            githubLink={projects[2].githubLink}
-            image={icons[2]}
-            alt={projects[2].alt}
-          />
-          
-          <Project 
-            name={projects[3].name}
-            projectLink={projects[3].projectLink}
-            githubLink={projects[3].githubLink}
-            image={icons[3]}
-            alt={projects[3].alt}
-          />
-          
-          <Project 
-            name={projects[4].name}
-            projectLink={projects[4].projectLink}
-            githubLink={projects[4].githubLink}
-            image={icons[4]}
-            alt={projects[4].alt}
-          />
-          
-          <Project 
-            name={projects[5].name}
-            projectLink={projects[5].projectLink}
-            githubLink={projects[5].githubLink}
-            image={icons[5]}
-            alt={projects[5].alt}
-          />
-          
-          <Project 
-            name={projects[6].name}
-            projectLink={projects[6].projectLink}
-            githubLink={projects[6].githubLink}
-            image={icons[6]}
-            alt={projects[6].alt}
-          />
-          
         </div>
       </div>
     </div>
